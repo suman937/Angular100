b8360eb27b3c6060723278b5bb281e854a3066cf
suman937 <suman937@yahoo.com>
2023-12-29 04:14:51 -0500

    #16 Property Binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,12 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  name='John Doe';
   product={
     name:'iPhoneX',
     price:789,
     color:'Black',
     discount:8.5,
-    inStock: 0
+    inStock: 10,
+    pImage:'/assets/images/iphone.png'
   }
 
   getDiscountedPrice(){
