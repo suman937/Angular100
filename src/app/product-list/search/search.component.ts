15de6de171102ffecf83f31af71b2015ebe93a3b
suman937 <suman937@yahoo.com>
2023-12-31 00:03:46 -0500

    #18 using *ngIf for Search results for div

--- src/app/product-list/search/search.component.ts
+++ src/app/product-list/search/search.component.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-  searchText:string = 'Mens wear';
-
+  searchText:string = 'Mens wear'; 
 
 
 }
