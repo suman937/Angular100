a51404a936b7989788843a03a91cd0ca2ca3e616
suman937 <suman937@yahoo.com>
2024-01-08 22:15:46 -0500

    #20 iterating through list using ngFor

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,42 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-   //name='John Doe';
-  addToCart: number= 0;
-  product={
-    name:'iPhoneX',
-    price:789,
-    color:'Black',
-    discount:8.5,
-    inStock: 10,
-    pImage:'/assets/images/iphone.png'
-  }
-
-  getDiscountedPrice(){
-   return this.product.price -  (this.product.price*this.product.discount /100)
-  }
-
-  decrementCartValue(){
-    if(this.addToCart>0){
-      this.addToCart--;
-    }
-   
-
-  }
-  incrementCartValue(){
-    if(this.addToCart<this.product.inStock){
-    this.addToCart++
-    }
-
-  }
-
-  onNameChange(event: any){
-    //this.name= event.target.value;
-    //console.log(event.target.value);
-  }
-  // name: string = 'iPhone 13';
-  // price: number = 999;
-  // color: string ='Red';
-  // discount: number = 
+   listOfStirng:string[]=['Mark','Steve','Mery','John','Sarah'];
 
 }
