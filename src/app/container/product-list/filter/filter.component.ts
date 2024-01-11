fef503fb98f51f8e67f4369be095be7aed1e1cdf
suman937 <suman937@yahoo.com>
2024-01-11 18:01:00 -0500

    #26 get the total, inStock and outOfStock count using input decorator

---
+++ src/app/container/product-list/filter/filter.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent {
+  @Input() all:number=0;
+  @Input() inStock:number=0;
+  @Input() outOfstock:number=0;
+
+}
