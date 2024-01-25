77791fab9a10edff3d4513b911506c52ab83e319
suman937 <suman937@yahoo.com>
2024-01-24 21:41:31 -0500

    #29 Template Reference Variable

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -7,11 +7,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SearchComponent {
   searchText:string = ''; 
+
 @Output()
   searchTextChanged:EventEmitter<string> = new EventEmitter<string>();
 
   onSearchTextChanged(){
+    
+  }
+
+  updateSearchText(inputEL:HTMLInputElement){
+    //this.onSearchTextChanged = event.target.value;
+    // console.log(inputEL.value);
+    this.searchText= inputEL.value;
+    // we are putting this line after we have set serchTxt property. if i put it before, in that case when the event is raised 
+    // by that time the serachText is empty string.
     this.searchTextChanged.emit(this.searchText);
+
   }
   
 
